80d8c38221baa0875da1f6e1e416480a4be88cc7
Atharva Deo <deovibhav@gmail.com>
2024-07-19 20:12:18 +0530

    Hyperparameter space generatiion script updates

--- src/create_hyperparameters_gbr.py
+++ src/create_hyperparameters_gbr.py
@@ -6,30 +6,32 @@ import os
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
+# https://medium.com/all-things-ai/in-depth-parameter-tuning-for-gradient-boosting-3363992e9bae (reference for selecting the  below values)[n_iter_no_change]
+# https://towardsdatascience.com/machine-learning-part-18-boosting-algorithms-gradient-boosting-in-python-ef5ae6965be4
+
 param_dist = {
     'loss': ['squared_error', 'absolute_error', 'huber', 'quantile'],
-    'learning_rate': uniform(0.0001, ), 
-    'n_estimators': randint(50, 300),
-    'subsample': uniform(0.6, 0.4), 
+    'learning_rate': uniform(0.001, 1.2),   # Learning rate of ML algorithms is usually between 0 to 1 
+    'n_estimators': randint(50, 500),   # General values used
+    'subsample': uniform(0.001, 1), 
     'criterion': ['friedman_mse', 'squared_error'],
-    'min_samples_split': uniform(0.01, 0.99), 
-    'min_samples_leaf': uniform(0.01, 0.99), 
+    'min_samples_split': uniform(0.001, 0.999), 
+    'min_samples_leaf': uniform(0.01, 0.999), 
     'min_weight_fraction_leaf': uniform(0.0, 0.5), 
-    'max_depth': [3, 5, None],
-    'min_impurity_decrease': uniform(0.0, 0.1), 
+    'max_depth': [1, 2, 3, 4, 5, None],
+    'min_impurity_decrease': uniform(0.0, 0.5), # As default is 0, selecting space closer to 0
     'init': [None, 'zero'],
-    'max_features': ['sqrt', 'log2', uniform(0.1, 0.9)], 
-    'alpha': uniform(0.1, 0.9), 
-    'verbose': [0, 1],
-    'max_leaf_nodes': [None, randint(10, 100)], 
+    'max_features': ['sqrt', 'log2', uniform(0.001, 0.999)], 
+    'alpha': uniform(0.001, 0.999),
+    'max_leaf_nodes': [None, randint(2, 50)],
     'warm_start': [False, True],
-    'validation_fraction': uniform(0.1, 0.3), 
+    'validation_fraction': uniform(0.001, 0.999), 
     'n_iter_no_change': [None, randint(5, 20)], 
-    'tol': uniform(1e-4, 1e-3),  
-    'ccp_alpha': uniform(0.0, 0.01)  
+    'tol': uniform(0.5e-4, 1e-3),  
+    'ccp_alpha': uniform(0.0, 0.02) # As default is 0, selecting space closer to 0
 }
 
-n_iter = 50
+n_iter = 1000
 
 random_search = list(ParameterSampler(param_dist, n_iter=n_iter, random_state=42))
 
